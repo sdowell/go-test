7a493f020849ca1940bac0f2901a498e261af3c1
Your <sdowell@google.com>
2022-03-04 00:33:03 +0000

    foo

--- greetings.go
+++ greetings.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
+	// foo
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
